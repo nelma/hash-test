6a7e5aaef2cff5ad05463379b37e81a48067fce2
Nelma da Silva Barros <nbarros@uolinc.com>
2019-11-11 10:08:34 -0300

    Atualizando documentação

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,28 +4,24 @@ const CalculatorController = require('../controller/calculator');
 const router = express.Router();
 
 /**
- * @api {get} /calculator Execute calc
+ * @api {post} /calculator Execute calc
  * @apiGroup Calculator
  *
- * @apiParam {Number} initial-amount
- * @apiParam {Number} interest
- * @apiParam {Number} period
+ * @apiParam {Number} grossValue
+ * @apiParam {Number} installments
+ * @apiParam {Number} MDR
  *
- *  @apiExample Example usage:
- * curl -X GET http://localhost:3000/api/calculator?totalValue=150.0&installments=1&MDR=4
- *
- * @apiSuccess {Number} totalValue
- * @apiSuccess {Number} installments
- * @apiSuccess {Number} MDR
+ * @apiExample Example usage:
+ * curl -X POST http://localhost:3000/api/calculator -H 'Content-Type: application/json' -d '{"grossValue": 120.0,"installments": 12,"MDR": 4}'
  *
  * @apiSuccessExample {json} Sucesso
  *    HTTP/1.1 200 OK
  *    {
  *      "result": {
- * 			tomorrow: 132,67,
- * 			in15Days: 135,36,
- * 			in30Days: 138,24,
- * 			in90Days: 144,00
+ * 			"tomorrow": 132,67,
+ * 			"in15Days": 135,36,
+ * 			"in30Days": 138,24,
+ * 			"in90Days": 144,00
  * 		},
  *      "violations": []
  *    }
